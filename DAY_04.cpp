f6dc81748b15f243738f15bca6b2bcdcae27a528
Aditya Rathore <116193915+RathoreAditya03@users.noreply.github.com>
2022-10-24 22:16:58 +0530

    Create DAY_04.cpp

---
+++ DAY_04.cpp
@@ -0,0 +1,40 @@
+// FRIEND CLASS PROGRAM
+#include <iostream>
+using namespace std;
+class A
+{
+  private:
+   int a1;
+  public:
+   A(int x=0)
+   {
+       a1=x;
+   }
+    friend class B;   
+};
+class B
+{
+  private:
+   int b1;
+  public:
+
+
+   B(int x=0)
+   {
+       b1=x;
+   }
+    int sum()
+    {
+        A a_obj(5);
+        
+        int sum = a_obj.a1 + b1;
+        cout<<"sum is: "<<sum;
+        return sum;
+    }
+};
+int main()
+{
+   B b_obj(10);
+   b_obj.sum();
+  return 0;
+}
