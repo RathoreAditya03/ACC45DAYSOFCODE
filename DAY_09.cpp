033bae647ce3354a7cc2f47ad0f0c0457c14c4bb
Aditya Rathore <116193915+RathoreAditya03@users.noreply.github.com>
2022-10-29 17:36:49 +0530

    Create DAY_09.cpp

---
+++ DAY_09.cpp
@@ -0,0 +1,31 @@
+// Defining a structure in cpp
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+struct person    //defination of structure
+{
+  string name;
+  int age;
+  float height;
+  float weight;
+  double salary;
+};
+
+int main()
+{
+    person p;
+    p.name = "Aditya";
+    p.age = 18;
+    p.weight = 56.6f;
+    p.height = 5.4f;
+    p.salary = 0;
+
+    cout<<endl<<"Name: "<<p.name;
+    cout<<endl<<"Age: "<<p.age;
+    cout<<endl<<"Height: "<<p.height;
+    cout<<endl<<"Weight: "<<p.weight;
+    cout<<endl<<"Salary: "<<p.salary;
+    return 0;
+  }
