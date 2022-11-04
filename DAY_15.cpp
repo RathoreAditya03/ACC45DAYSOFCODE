6d556a6a320ad06dd9c3ec848383a216903777ed
Aditya Rathore <116193915+RathoreAditya03@users.noreply.github.com>
2022-11-04 21:57:19 +0530

    Create DAY_15.cpp

---
+++ DAY_15.cpp
@@ -0,0 +1,14 @@
+// C++ program to print all natural numbers upto
+
+#include<iostream>
+
+using namespace std;
+int N = 10;
+
+int main()
+{
+static int x = 1;
+if (cout << x << " " && x++ < N && main())
+{ }
+return 0;
+}
