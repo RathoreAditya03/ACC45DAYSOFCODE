8024660e6957485586a4876a8571f3a0032dd968
Aditya Rathore <116193915+RathoreAditya03@users.noreply.github.com>
2022-11-08 15:20:17 +0530

    Create DAY_19.cpp

---
+++ DAY_19.cpp
@@ -0,0 +1,12 @@
+// CPP program to print sum of two integers without using +
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int a = 5;
+int b = 5;
+int sum = -( -a-b );
+cout << sum;
+return 0;
+}
