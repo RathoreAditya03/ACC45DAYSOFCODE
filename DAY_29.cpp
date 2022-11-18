bb4d033f159d7db5b1d0af711b3f7f6b6106321d
Aditya Rathore <116193915+RathoreAditya03@users.noreply.github.com>
2022-11-18 18:55:19 +0530

    Create DAY_29.cpp

---
+++ DAY_29.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  string name[3] = {"Aditya" , "Abhay" , "Amit"};   
+  for(int i=0; i<3; i++)
+  {
+    cout<<endl<<name[i];
+  }
+  return 0;
+}
